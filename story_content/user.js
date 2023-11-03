24ae479a4065ec85f88707b30fd963c5f375e36c
Asimut <asimut007@gmail.com>
2023-11-03 14:22:59 +0200

    Upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,13 +2,51 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5hmSGJJ0bBA":
+      case "68CykBxTGxE":
         Script1();
+        break;
+      case "6NIlVlgvneD":
+        Script2();
         break;
   }
 }
 
 function Script1()
+{
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+
+(document.mozFullScreen && !document.webkitisFullScreen)) {
+
+if (document.documentElement.requestFullScreen) (
+
+document.documentElement.requestFullscreen(); } else if (document.documentElement.mozRequestFullScreen) {
+
+document.documentElement.mozRequestFullScreen0:
+
+} else if (document.documentElement.webkitRequestFullScreen) {
+
+document.documentElement.webkitRequestFullScreen
+
+(Element.ALLOW KEYBOARD_INPUT)
+
+} else {
+
+if (document.cancelFullScreen) (
+
+document.cancelFullScreen();
+
+} else if (document.mozCancelFullScreen) {
+
+document.mozCancelFullScreen();
+
+} else if (document.webkitCancelFullScreen) {
+ document.webkitCancelFullScreen();
+
+}
+}
+}
+
+function Script2()
 {
   if ((document.fullScreenElement && document.fullScreenElement !== null) ||
 
